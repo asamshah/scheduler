364046b0784821efbca23d5c6730ab0753e8616d
Asam Shah <asamshah@gmail.com>
2021-05-12 20:59:44 +0100

    User Authentication

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,7 @@
 class MainController < ApplicationController
 
     def index
-        flash[:notice] = "Logged In Successfully"
-        flash[:alert] = "Wrong! Try again."
+ 
     end
     
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    def new
+        
+    end
+    
+    def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "You're logged in"
+        else
+            flash[:alert] = "Nope. Try again"
+            render :new
+        end
+    end
+    
+
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_path, notice: "Logged out"
+    end
+end
