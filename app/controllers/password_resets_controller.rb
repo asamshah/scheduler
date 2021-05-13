eceda752a6eccd18b3e1b013d7c18d8953786494
Asam Shah <asamshah@gmail.com>
2021-05-13 21:21:05 +0100

    Credentials Added

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,6 +14,27 @@ class PasswordResetsController < ApplicationController
         
     end
     
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Token expired. Please try again."
+    end
+    
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password has been reset. Please sign in."
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+    
     
 
 end
